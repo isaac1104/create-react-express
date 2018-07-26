365abed4933ef64ec353c2aa822b2254024eabe5
Isaac Kwon <isaac-kwon@hotmail.com>
2018-07-26 09:01:29 -0700

    welcome msg change

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to React!</h1>
+        <h1>Welcome to React/Redux Boilerplate with Express</h1>
       </div>
     );
   }

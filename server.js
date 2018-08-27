7fc3e9836cb04d9fb25eb8cb5ee5444266150c2b
Isaac Kwon <isaac-kwon@hotmail.com>
2018-08-27 16:11:00 -0700

    refactor proxy route in package.json

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+require('./routes/test')(app);
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
 });

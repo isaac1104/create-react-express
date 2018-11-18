576d50762d29774c8bb029a85748435f2a5b9a8a
isaac1104 <isaac-kwon@hotmail.com>
2018-11-17 20:36:00 -0800

    add passport.js and config folder

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongodbURI: process.env.MONGODB_URI,
+  cookieKey: process.env.COOKIEKEY,
+};
---
+++ utils/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const keys = require('../config/keys');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accesstoken, refreshToken, profile, done) => {
+      const currentUser = await User.findOne({ googleId: profile.id });
+      if (currentUser) {
+        return done(null, currentUser);
+      }
+      const user = await new User({
+        googleId: profile.id,
+        username: profile.displayName,
+        avatar: profile.photos[0].value || ''
+      }).save();
+      done(null, user);
+    }
+  )
+);

f34dea0b16e95b3786421f551a7e7139c225e634
isaac1104 <isaac-kwon@hotmail.com>
2018-11-17 20:36:44 -0800

    add User model

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+  username: String,
+  avatar: String
+});
+
+mongoose.model('User', userSchema);

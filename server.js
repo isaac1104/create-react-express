f653c686ac9973f2d204a443f520dfc8fb82cc6a
isaac1104 <isaac-kwon@hotmail.com>
2018-11-17 20:39:03 -0800

    config server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
+require('./models/User');
+require('./utils/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongodbURI);
 
 app.use(express.static('client/public'));
 
@@ -13,6 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 require('./routes/test_route')(app);
+require('./routes/auth_route')(app);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));

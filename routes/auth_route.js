74ba883c29bb7c2d5e9bd61d68731d8020659cab
isaac1104 <isaac-kwon@hotmail.com>
2018-11-17 20:37:09 -0800

    add auth route

---
+++ routes/auth_route.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/home');
+  });
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+  app.get('/api/signout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+};

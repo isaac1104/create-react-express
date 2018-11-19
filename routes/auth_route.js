8616a8a84cb84bff80078c1725cbfddfab64581e
isaac1104 <isaac-kwon@hotmail.com>
2018-11-18 16:38:33 -0800

    change redirect route after oauth

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 module.exports = app => {
   app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-    res.redirect('/home');
+    // Change the redirect route if needed
+    res.redirect('/');
   });
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);

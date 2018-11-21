a52f595878a2a3d24a9f30feb35acb3f93a8a7ec
Isaac Kwon <isaac-kwon@hotmail.com>
2018-11-21 15:13:59 -0800

    restyle landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const styles = {
+      container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#282c34',
+        color: '#ffffff',
+        textAlign: 'center'
+      }
+    };
+
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh', textAlign: 'center' }}>
+      <div style={styles.container}>
         <div>
-          <h1><code>Create-React-App</code> with <code>Redux</code>, <code>Node</code> and <code>Express</code> + <code>Google OAuth</code></h1>
-          <h2>Modify <code>App.js</code> to get started!</h2>
-          <h2>Open <code>server.js</code> to see how to configure <code>dev.js</code> file</h2>
+          <h1>Welcome to Fullstack React Boilerplate with <code>Node</code> and <code>Express</code></h1>
+          <h3>Edit <code>client/src/components/App.js</code> and save to reload.</h3>
+          <h3>You can also authenticate users using Google OAuth by directing them to <code>/auth/google</code></h3>
+          <p>Please check inside of the two <code>package.json</code> files to see the list of dependencies that are being used.</p>
         </div>
       </div>
     );

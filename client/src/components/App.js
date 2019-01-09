24ff8ebd702b7957cddaf7fb81f94c77b9a22900
isaac1104 <isaac-kwon@hotmail.com>
2019-01-08 20:55:57 -0800

    wrap emoji in span

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     const Landing = () => (
       <div style={styles.container}>
         <div>
-          <h1>💻 Welcome to Fullstack React Boilerplate</h1>
+          <h1><span role='img' aria-label='computer-emoji'>💻</span> Welcome to Fullstack React Boilerplate</h1>
           <h3>Edit <code>client/src/components/App.js</code> and save to reload.</h3>
           <h3>Direct the users to <code>/auth/google</code> to authenticate them with Google.</h3>
           <h3>See <code>package.json</code> for the full list of dependencies.</h3>

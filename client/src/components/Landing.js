856764c66490101916373da4b0becd7d3cefa99e
isaac1104 <isaac-kwon@hotmail.com>
2019-05-11 23:11:57 +0900

    create Landing.js for easier routing

---
+++ client/src/components/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#282c34',
+    color: '#ffffff',
+    textAlign: 'center'
+  }
+};
+
+const Landing = () => (
+  <div style={styles.container}>
+    <div>
+      <h1><span role='img' aria-label='computer-emoji'>💻</span> Welcome to Fullstack React Boilerplate</h1>
+      <h3>Edit <code>client/src/components/App.js</code> and save to reload.</h3>
+      <h3>Direct the users to <code>/auth/google</code> to authenticate them with Google.</h3>
+      <h3>See <code>package.json</code> for the full list of dependencies.</h3>
+    </div>
+  </div>
+);
+
+export default Landing;

548b08f2c172548f2a923c2509589e4b5c39d2e3
isaac1104 <isaac-kwon@hotmail.com>
2019-06-25 23:32:56 +0900

    update npm packages

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -4,7 +4,7 @@ import styles from './Landing.module.css';
 const Landing = () => (
   <div className={styles.LandingContainer}>
     <div>
-      <h1><span role='img' aria-label='computer-emoji'>💻</span> Welcome to Fullstack React Boilerplate</h1>
+      <h1><span role='img' aria-label='computer-emoji'>💻</span> Welcome to MERN Stack Boilerplate</h1>
       <h3>Edit <code>client/src/components/App.js</code> and save to reload.</h3>
       <h3>See <code>package.json</code> for the full list of dependencies.</h3>
       <h3>Click <a href='https://github.com/isaac1104/create-react-express'>here</a> for documentation</h3>

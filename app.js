0271d5bb3071b71529023d7d8e8459ddff9fcadf
isaac1104 <isaac-kwon@hotmail.com>
2020-03-16 18:11:35 +0900

    refactor

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
 const express = require('express');
-const passport = require('passport');
 const helmet = require('helmet');
-const cookieSession = require('cookie-session');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-require('./models/User');
-require('./services/passport');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongodbURI, { useNewUrlParser: true });
-
 app.use(helmet());
 app.use(express.json());
-app.use(cookieSession({
-  name: 'session',
-  maxAge: 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey]
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 require('./routes/test_route')(app);
-require('./routes/auth_route')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongodbURI: process.env.MONGODB_URI,
-  cookieKey: process.env.COOKIE_KEY,
-};

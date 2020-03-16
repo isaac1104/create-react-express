f1a3e76b6c331f66875ccb73b1a8defd4a7c1f51
isaac1104 <isaac-kwon@hotmail.com>
2020-03-16 18:28:21 +0900

    upgrade npm packages

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(proxy(['/api'], { target: 'http://localhost:5000' }));
+  app.use(createProxyMiddleware(['/api'], { target: 'http://localhost:5000' }));
 };
